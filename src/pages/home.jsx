6f64b4b0ae9aa71ac795745c5cfd99b5a4caa367
Lina Marcela <linavilla.2@gmail.com>
2022-05-18 18:21:17 -0500

    se agregan estilos directos al Link

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ import image from '../assets/logo.png'
 
 export default function Home() {
   return (
-      <div className='container-home'>
+      <div className= 'container-home'>
         <header className='home-header'>
           <img className='home-header-logo' src={image} alt="logotipo" />
           <p className='home-header-button'>
