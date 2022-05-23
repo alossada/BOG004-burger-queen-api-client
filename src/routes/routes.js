6c1a70f53c405ce3dace515c6e7769646cba3cea
unknown <alossada@gmail.com>
2022-05-23 12:15:05 -0500

    se modifica el router eliminando componentes de usuario y dejando unicamente el ruteo por rol

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,25 +1,17 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
-//-------- importamos las vistas -------- 👇
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+//-------- importamos las vistas -------- :apuntando_hacia_abajo:
 import Home from '../pages/home';
-import Login from '../pages/login';
-import Chef from '../pages/chef';
-import Waiter from '../pages/waiter';
-import Admin from '../pages/admin';
-
+import LoginForm from '../pages/login';
+import { RouterByRoles } from './routerByRoles';
 const Rutas = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />}></Route>
-                <Route path='login' element={<Login/>}></Route>
-                <Route path='login/waiter' element={<Waiter />}></Route>
-                <Route path='login/chef' element={<Chef />}></Route>
-                <Route path='login/admin' element={<Admin />}></Route>
-                <Route path='*' element={<Navigate to={"/"} />}></Route>
+                <Route path='login' element={<LoginForm/>}></Route>
+                <Route path='/*' element={<RouterByRoles/>} />
             </Routes>
         </BrowserRouter>
     );
 }
-
 export default Rutas;
