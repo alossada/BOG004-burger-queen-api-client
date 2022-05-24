071d2936aba098d9e25d8c2ef2c77aecdde31205
unknown <alossada@gmail.com>
2022-05-24 15:51:07 -0500

    Se crea archivo para estructura de producto

---
+++ src/components/product.jsx
@@ -0,0 +1,17 @@
+
+const Product = (props) => {
+    const onClick = () => {
+        props.handleAddProduct({name: props.name, price: props.price, id: props.id, type: props.type});
+    }
+    
+    return (
+        <div className='product' onClick={onClick}>
+            <img src={props.image} alt={props.name} />
+            <p>{props.name}</p>
+            <p>${props.price}</p>
+            <p>{props.type}</p>
+        </div>
+    );
+};
+
+export default Product;
