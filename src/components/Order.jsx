b83923484a5af1d40ce7b7067118207524d2184b
unknown <alossada@gmail.com>
2022-05-24 15:52:55 -0500

    Se importan hooks y se crean funciones para el llamado de la peticion

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { products } from '../Providers/OrderPetitions'
+import { getToken } from '../Providers/UserPetitions';
 
-export default function Order() {
+export default function Order({handleAddProduct}) {
+
+  const [productos, setProductos] = useState([]); 
+  console.log('Un banderita', sessionStorage);
+  const token = getToken();
+  console.log('getToken', token.accessToken);
+
+  useEffect(()=>{
+    products(token.accessToken) // llamamos a la función products() que está en el provider
+      .then((response) => {
+        console.log(sessionStorage);
+        console.log('Esto es la respuesta del GET', response)
+        setProductos(response.data); // actualizamos el estado  
+        console.log('Set de productos', setProductos(response.data));      
+      })
+      .catch(() => {});
+  }, []);
+
+
   return (
     <div>
       <h2 style={{color:'#bde0fe'}}>menu de ordenes</h2>
