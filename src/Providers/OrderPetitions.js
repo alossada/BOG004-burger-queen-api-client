cfcff26adbb9a71b973ae11c5d06aac245b606b9
unknown <alossada@gmail.com>
2022-05-24 15:50:29 -0500

    Se crea archivo para realizar peticion para ordenes

---
+++ src/Providers/OrderPetitions.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { getToken } from "./UserPetitions";
+const url = 'http://localhost:8080/';
+
+const products = (token) => {
+
+    return axios({
+        method: "GET", 
+        url:url+'products', 
+        headers: {
+            'content-type': 'application/json',
+            authorization: 'Bearer ' + token,
+            },
+      });
+ } 
+
+ export { products }
