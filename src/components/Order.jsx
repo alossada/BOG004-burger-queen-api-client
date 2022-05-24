4ac90a434d7df19579fd1a8ea7ea8427ff17759d
Lina Marcela <linavilla.2@gmail.com>
2022-05-24 18:16:03 -0500

    obtenemos la lista de productos resolviendo la peticion

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -6,25 +6,28 @@ import { getToken } from '../Providers/UserPetitions';
 export default function Order({handleAddProduct}) {
 
   const [productos, setProductos] = useState([]); 
-  console.log('Un banderita', sessionStorage);
   const token = getToken();
-  console.log('getToken', token.accessToken);
+  // console.log('getToken', token.accessToken);
 
   useEffect(()=>{
     products(token.accessToken) // llamamos a la función products() que está en el provider
       .then((response) => {
-        console.log(sessionStorage);
-        console.log('Esto es la respuesta del GET', response)
-        setProductos(response.data); // actualizamos el estado  
-        console.log('Set de productos', setProductos(response.data));      
+        // console.log('Esto es la respuesta del GET', response)
+        setProductos(response.data); // actualizamos el estado 
+        // console.log('respuesta de la data',response.data) 
       })
       .catch(() => {});
-  }, []);
+  },[]);
 
+  const resultProduct = () =>{
+    console.log('Productos:',productos)
+  }
+  resultProduct()
 
   return (
     <div>
       <h2 style={{color:'#bde0fe'}}>menu de ordenes</h2>
+      {/* <p>{productos[0].name}</p> */}
     </div>
   )
 }
