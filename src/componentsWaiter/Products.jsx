226a0b90268fe3d471f8b43a112494196fc48ac7
Lina Marcela <linavilla.2@gmail.com>
2022-05-26 17:22:06 -0500

    se agrega index como parametro en filterproducts

--- src/componentsWaiter/Products.jsx
+++ src/componentsWaiter/Products.jsx
@@ -3,37 +3,35 @@ import { useEffect, useState } from 'react'
 import { products } from '../Providers/OrderPetitions'
 import { getToken } from '../Providers/UserPetitions'
 import Menu from './Menu'
+import Style from '../styles/waiter.module.css'
 
 export default function ProductsMenus({handleAddProduct}) {
 
   const [FilterProducts, setFilterProducts] =useState([]);
   const token = getToken();
-  // console.log('getToken', token.accessToken);
 
   useEffect(()=>{
     products(token.accessToken) // llamamos a la función products() que está en el provider
       .then((response) => {
-        // console.log('Esto es la respuesta del GET', response)
         setFilterProducts(response.data)
-        // actualizamos el estado 
-        // console.log('respuesta de la data',response.data) 
       })
       .catch(() => {});
   },[]);
 
     return (
-      <div className='allProducts'>
-      {FilterProducts.map((products) => {
+      <div className={Style.allProducts}>
+      {FilterProducts.map((products, index) => {
         return(
           <div className='containerList'>
             <Menu
-              key={"ord" + products.id}
+              key={index}
               id={products.id}
               handleAddProduct={handleAddProduct}
               name={products.name}
               price={products.price}
               image={products.image}
               type={products.type}
+              item={products}
             />
           </div>
         )
