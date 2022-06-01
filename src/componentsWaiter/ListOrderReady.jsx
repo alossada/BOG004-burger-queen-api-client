ae10dfb1b27f6a203b6cdc609e946693a893a844
Lina Marcela <linavilla.2@gmail.com>
2022-06-01 11:37:39 -0500

    se cambia h2 por h3

--- src/componentsWaiter/ListOrderReady.jsx
+++ src/componentsWaiter/ListOrderReady.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function OrdenReady() {
   return (
     <div>
-        <h2 style={{color:'#90e0ef'}}>Seccion 2. Pedidos listos</h2>
+        <h3 style={{color:'#90e0ef'}}>Seccion 2. Pedidos listos</h3>
     </div>
   )
 }
