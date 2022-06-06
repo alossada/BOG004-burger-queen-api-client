d18e85072c3e6414f611518ac612c555a2eaf15b
unknown <alossada@gmail.com>
2022-06-06 17:05:57 -0500

    Se cambio el nombre de las props para mas claridad

--- src/componentsChef/OrderStructure.jsx
+++ src/componentsChef/OrderStructure.jsx
@@ -2,21 +2,19 @@ import React from 'react'
 import Items from './Items'
 import Style from '../styles/chef.module.css'
 
-export default function OrderStructure(props) {
-    console.log('soy props', props)
+export default function OrderStructure({totalOrders}) {
   return (
     <div>
       <div className={Style.containerOrders}>
         <div className={Style.nameDate}>
-          <p>{props.cliente}</p>
-          <p>{props.date}</p>
+          <p>{totalOrders.client}</p>
+          <p>{totalOrders.dataEntry}</p>
         </div>
         <h5>Pedido:</h5>
           <Items 
-              products = {props.items.products}
+            products = {totalOrders.products}
           />
-          <button className={Style.btnListo} type='submit'>Listo</button>
-          {/* <p style={{color:'#FE8D06', fontWeight:'700'}}>{props.items.status}</p> */}
+          <button className={Style.btnListo} type='submit'>Listo</button>          
       </div>
     </div>
   )
