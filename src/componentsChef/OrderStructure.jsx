dba561c5eb0f1e356b6aa3f7ef823158f7817fdc
unknown <alossada@gmail.com>
2022-06-09 12:47:15 -0500

    se eliminan comentarios

--- src/componentsChef/OrderStructure.jsx
+++ src/componentsChef/OrderStructure.jsx
@@ -4,26 +4,11 @@ import Style from '../styles/chef.module.css'
 import { orderStatus } from '../Providers/OrderPetitions'
 import { getToken  } from '../Providers/UserPetitions'
 
-// const getDate = () => {
-//   let rightDate = new Date();
-//   return rightDate.getFullYear() +
-//       '-' +
-//       (rightDate.getMonth() + 1 )+
-//       '-' +
-//       rightDate.getDate() +
-//       ' ' +
-//       rightDate.getHours() +
-//       ':' +
-//       rightDate.getMinutes()    
-// }
-
 
 export default function OrderStructure({totalOrders}) {
-  console.log('FECAH ORDEN:',new Date(totalOrders.dataEntry).toLocaleString('sv'));
-  // console.log('FECHA ACTUAL', getDate().toLocaleString('sv'))
-  console.log('Resta de Fechas', new Date(totalOrders.dataEntry))
-  const token = getToken();
-  const newStatus = (orderId) => {
+
+const token = getToken();
+const newStatus = (orderId) => {
     orderStatus(orderId, token.accessToken)
     .then((response)=>{
       return response
@@ -38,7 +23,7 @@ export default function OrderStructure({totalOrders}) {
       <div className={Style.containerOrders}>
         <div className={Style.nameDate}>
           <p>{totalOrders.client}</p>
-          <p>{totalOrders.dataEntry}</p>
+          <p>{totalOrders.dateEntry}</p>
         </div>
         <h5>Pedido:</h5>
           <Items 
