d3f27f283d83467910fc7a5c24fd7ff6c0dd8b46
Lina Marcela <linavilla.2@gmail.com>
2022-06-09 11:32:21 -0500

    se comenta el getDate()

--- src/componentsChef/OrderStructure.jsx
+++ src/componentsChef/OrderStructure.jsx
@@ -4,11 +4,25 @@ import Style from '../styles/chef.module.css'
 import { orderStatus } from '../Providers/OrderPetitions'
 import { getToken  } from '../Providers/UserPetitions'
 
+// const getDate = () => {
+//   let rightDate = new Date();
+//   return rightDate.getFullYear() +
+//       '-' +
+//       (rightDate.getMonth() + 1 )+
+//       '-' +
+//       rightDate.getDate() +
+//       ' ' +
+//       rightDate.getHours() +
+//       ':' +
+//       rightDate.getMinutes()    
+// }
+
 
 export default function OrderStructure({totalOrders}) {
-  
+  console.log('FECAH ORDEN:',new Date(totalOrders.dataEntry).toLocaleString('sv'));
+  // console.log('FECHA ACTUAL', getDate().toLocaleString('sv'))
+  console.log('Resta de Fechas', new Date(totalOrders.dataEntry))
   const token = getToken();
-    
   const newStatus = (orderId) => {
     orderStatus(orderId, token.accessToken)
     .then((response)=>{
@@ -33,6 +47,7 @@ export default function OrderStructure({totalOrders}) {
           />
           <button
             type='button'
+            id='button'
             className={Style.btnListo}
             onClick={() =>newStatus(totalOrders.id)}
           >Listo</button>
