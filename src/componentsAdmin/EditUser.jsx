d277ac08e76556888d83e64768ec3515edc9ccd1
unknown <alossada@gmail.com>
2022-06-13 18:18:52 -0500

    Se crea componente con funcion para eliminar usuarios

--- src/componentsAdmin/EditUser.jsx
+++ src/componentsAdmin/EditUser.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
-export default function EditUser() {
+export default function EditUser(props) {
+  //console.log(props.currentUser)
+
+  const {register, handleSubmit, formState: { errors }, setValue} = useForm({
+    defaultValues:props.currentUser
+  });
+
+  setValue('name',props.currentUser.name);
+  setValue('username',props.currentUser.username);
+
+  const onSubmit = (data, e) => {
+    //console.log(data)
+    data.id = props.currentUser.id
+  
+    props.updateUser(props.currentUser.id, data)
+    
+    //limpia los campos
+    e.target.reset();
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Nombre</label>
+      <input type="text" name="name" {...register("name", {
+                required: {value: true, message: 'Campo Requerido'}
+            })}
+      />
+      <div>
+        {errors?.name?.message}
+      </div>
+      <label>Rol</label>
+      <input type="text" name="username" {...register("username", {
+            required: {value: true, message: 'Campo Requerido'}
+        })} />
+      <div>
+        {errors?.usarname?.message}
+      </div>
+      <button>Editar</button>
+    </form>
   )
 }
