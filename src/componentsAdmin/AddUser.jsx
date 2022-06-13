92988181406be5417456bc001e56324167552ae7
unknown <alossada@gmail.com>
2022-06-13 18:19:24 -0500

    Se crea componente para agregar usuarios

--- src/componentsAdmin/AddUser.jsx
+++ src/componentsAdmin/AddUser.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
-export default function Adduser() {
+export default function Adduser(props) {
+  const {register, handleSubmit, formState: { errors }} = useForm();
+
+  const onSubmit = (data, e) => {
+    //console.log(data)
+    props.addUser(data)
+    
+    //limpia los campos
+    e.target.reset();
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>correo</label>
+      <input type="text" name="email" {...register("email", {
+                required: {value: true, message: 'Campo Requerido'}
+            })}
+       />
+      <div>
+        {errors?.email?.message}
+      </div>
+      <label>contraseña</label>
+      <input type="text" name="passewordca" {...register("passewordca", {
+                required: {value: true, message: 'Campo Requerido'}
+            })}
+       />
+      <div>
+        {errors?.passewordca?.message}
+      </div>
+      <label>Rol</label>
+      <input type="text" name="rol" {...register("rol", {
+            required: {value: true, message: 'Campo Requerido'}
+        })} />
+      <div>
+        {errors?.rol?.message}
+      </div>
+      <button>Agregar</button>
+    </form>
   )
 }
