e955c4f5a56d1e1f52d40ab0855e8d0c4706e688
unknown <alossada@gmail.com>
2022-06-13 18:17:31 -0500

    Se llama componente adminController

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
-import UserTable from '../componentsAdmin/UserTable'
-import Style from '../styles/admin.module.css'
+import AdminController from '../componentsAdmin/AdminController'
+//import Style from '../styles/admin.module.css'
 
 
 export default function Admin({ id }) {
   
   return (
-    <div className={ Style.container}>
-      <h1>Administrador</h1>
-      <div className={Style.flex_row}>
-        <div className={Style.flex_large}>
-          <h2>Agregar Usuario</h2>
-        </div>
-        <div className={Style.flex_large}>
-          <h2>Ver Usuarios</h2>
-          <UserTable />
-        </div>
-      </div>
+    <div>
+      <AdminController />
     </div>
   )
 }
