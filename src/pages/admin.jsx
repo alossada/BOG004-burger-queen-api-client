0f73557263167a58c6fb3678e31a5f8c93343af4
unknown <alossada@gmail.com>
2022-06-13 13:10:57 -0500

    Se estructura render y se llama componente UserTable

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import UserTable from '../componentsAdmin/UserTable'
+import Style from '../styles/admin.module.css'
+
 
 export default function Admin({ id }) {
   
   return (
-    <div>
-      <h1 style={{color: "#f1f1f1",  fontSize: 16, fontWeight:'bold'}}>Hola soy admin</h1>
+    <div className={ Style.container}>
+      <h1>Administrador</h1>
+      <div className={Style.flex_row}>
+        <div className={Style.flex_large}>
+          <h2>Agregar Usuario</h2>
+        </div>
+        <div className={Style.flex_large}>
+          <h2>Ver Usuarios</h2>
+          <UserTable />
+        </div>
+      </div>
     </div>
   )
 }
