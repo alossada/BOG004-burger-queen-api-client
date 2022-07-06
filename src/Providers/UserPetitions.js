bae6f69b08484136eb447561998021c856afc0cc
unknown <alossada@gmail.com>
2022-07-06 10:39:25 -0500

    se agrega variable de ambiente y de desarrollo

--- src/Providers/UserPetitions.js
+++ src/Providers/UserPetitions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'http://localhost:8080/';
+const url = process.env.REACT_APP_API_URL;
 
 const login = (data) => {
     return axios.post(url+'login', data);
